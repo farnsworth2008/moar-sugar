0599f514ddaafc934a870f3eff562e23a5f844f3
Mark Farnsworth <farnsworth2008@gmail.com>
2019-03-26 12:22:53 -0400

    GetBoolean

---
+++ src/main/java/moar/sugar/GetBoolean.java
@@ -0,0 +1,13 @@
+package moar.sugar;
+
+/**
+ * An interface to provide a value.
+ *
+ * @author Mark Farnsworth
+ */
+public interface GetBoolean {
+  /**
+   * @return value
+   */
+  Boolean getBoolean();
+}
